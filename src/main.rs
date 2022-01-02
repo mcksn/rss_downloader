c5d9b76fbf99d2b471becb0dc495208cdbd18e3e
mcksn <mackson.ogu@gmail.com>
2022-01-02 19:45:26 +0000

    attempting to pull mp3 links

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+// use std::collections::HashMap;
+extern crate feed;
+extern crate rss;
+
+use feed::{ChannelGetters, EnclosureGetters, FromUrl, ItemGetters};
+use rss::Channel;
+
 fn main() {
-    println!("Hello, world!");
+    let url = "";
+
+    let channel = Channel::from_url(url).unwrap();
+    println!(
+        "Feed Title: {:?}",
+        channel
+            .items()
+            .iter()
+            .filter(|i| i.title().unwrap().contains("Alien"))
+            .next()
+            .unwrap()
+            .enclosure()
+            .unwrap()
+            .url()
+    );
 }
